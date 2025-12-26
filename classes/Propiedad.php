fe1b050828887fd8dffee28f1c39aa26a663599a
araque1990 <araque1990.cdap@gmail.com>
2025-12-26 18:35:29 +0100

    feat: Incluir validación en clase Propiedad

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -21,4 +21,41 @@ class Propiedad extends ActiveRecord
     ) {
         $this->creado = date('Y/m/d');
     }
+
+    public function validar() {
+
+        if(!$this->titulo) {
+            self::$errores[] = "Debes añadir un titulo";
+        }
+
+        if(!$this->precio) {
+            self::$errores[] = 'El Precio es Obligatorio';
+        }
+
+        if( strlen( $this->descripcion ) < 50 ) {
+            self::$errores[] = 'La descripción es obligatoria y debe tener al menos 50 caracteres';
+        }
+
+        if(!$this->habitaciones) {
+            self::$errores[] = 'El Número de habitaciones es obligatorio';
+        }
+
+        if(!$this->wc) {
+            self::$errores[] = 'El Número de Baños es obligatorio';
+        }
+
+        if(!$this->estacionamiento) {
+            self::$errores[] = 'El Número de lugares de Estacionamiento es obligatorio';
+        }
+
+        if(!$this->vendedorId) {
+            self::$errores[] = 'Elige un vendedor';
+        }
+
+        if(!$this->imagen ) {
+            self::$errores[] = 'La Imagen es Obligatoria';
+        }
+
+        return self::$errores;
+    }
 }
